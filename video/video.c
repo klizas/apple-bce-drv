bfedc3513a7c53f2ef37d53ea38a7125e1c88bab
klizas <lukas@klizas.lt>
2026-04-17 01:36:12 +0300

    video: drop redundant vb2 wait_prepare/wait_finish ops

--- video/video.c
+++ video/video.c
@@ -701,8 +701,6 @@ static const struct vb2_ops ave_vb2_ops = {
 	.buf_queue		= ave_buf_queue,
 	.start_streaming	= ave_start_streaming,
 	.stop_streaming		= ave_stop_streaming,
-	.wait_prepare		= vb2_ops_wait_prepare,
-	.wait_finish		= vb2_ops_wait_finish,
 };
 
 /* === M2M ops === */
