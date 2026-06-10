3eb77a6b526f94be79a3ade18be7f4778da9d017
klizas <lukas@klizas.lt>
2026-06-10 22:57:13 +0300

    aaudio: ignore timestamps for subdevices without a PCM

--- audio/pcm.c
+++ audio/pcm.c
@@ -353,6 +353,8 @@ void aaudio_handle_timestamp(struct aaudio_subdevice *sdev, ktime_t os_timestamp
 {
     struct snd_pcm_substream *substream;
 
+    if (!sdev->pcm)
+        return;
     substream = sdev->pcm->streams[SNDRV_PCM_STREAM_PLAYBACK].substream;
     if (substream)
         aaudio_handle_stream_timestamp(substream, dev_timestamp);
